22b63c2fbc6ea56ee660fd180051d99ea5e5c15b
Liliana Ferrer <40367237+Lefs11@users.noreply.github.com>
2023-05-11 11:27:24 -0600

    Datatable updates

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -40,14 +42,18 @@ namespace BulkyBook.Models
         [Range(1, 10000)]
         public double Price100 { get; set; }
 
+        [ValidateNever]
         public string ImageUrl { get; set; }
 
         [Required]
         public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }
 
         [Required]
         public int CoverTypeId { get; set; }
+        [ValidateNever]
         public CoverType CoverType { get; set; }
     }
 }
