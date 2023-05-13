4f64cee89810f995048d6352956521a2d0350a81
Liliana Ferrer <40367237+Lefs11@users.noreply.github.com>
2023-05-12 18:37:34 -0600

    Delete function added

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -28,17 +28,21 @@ namespace BulkyBook.Models
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "List Price")]
         public double ListPrice { get; set; }
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "Price for 1-50")]
         public double Price { get; set; }
 
         [Required]
+        [Display(Name = "Price for 51-100")]
         [Range(1, 10000)]
         public double Price50 { get; set; }
 
         [Required]
+        [Display(Name = "Price for 100+")]
         [Range(1, 10000)]
         public double Price100 { get; set; }
 
@@ -46,12 +50,14 @@ namespace BulkyBook.Models
         public string ImageUrl { get; set; }
 
         [Required]
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         [ValidateNever]
         public Category Category { get; set; }
 
         [Required]
+        [Display(Name = "Cover Type")]
         public int CoverTypeId { get; set; }
         [ValidateNever]
         public CoverType CoverType { get; set; }
