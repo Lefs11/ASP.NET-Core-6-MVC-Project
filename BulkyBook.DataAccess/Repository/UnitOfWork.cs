22c00f7c5cee3a66b1f8176fbe7ee88ea76d1bde
Liliana Ferrer <40367237+Lefs11@users.noreply.github.com>
2023-05-27 15:02:33 -0600

    Add items to shopping cart

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -16,12 +16,16 @@ namespace BulkyBook.DataAccess.Repository
             Cateogry = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
-            Company= new CompanyRepository(_db);
+            Company = new CompanyRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
         }
         public ICateogryRepository Cateogry { get; private set; }
         public ICoverTypeRepository CoverType { get; private set; }
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
 
         public void Save()
         {
