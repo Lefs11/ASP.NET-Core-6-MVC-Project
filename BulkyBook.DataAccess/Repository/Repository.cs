287fea5c0f757d89fb4f2f8f72a13e9da1863d8b
Liliana Ferrer <40367237+Lefs11@users.noreply.github.com>
2023-05-29 18:13:50 -0600

    ShoppingCartVM added

--- BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -26,10 +26,11 @@ namespace BulkyBook.DataAccess.Repository
         }
 
         //includeProperties - "Category, CoverType"
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
-            if(includeProperties != null)
+            query = query.Where(filter);
+            if (includeProperties != null)
             {
                 foreach(var includeProp in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
                 {
