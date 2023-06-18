09603f77725ba5995111ec80c8da0666223d789f
Liliana Ferrer <40367237+Lefs11@users.noreply.github.com>
2023-06-18 11:13:22 -0600

    SendGrip for emails added

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,6 +1,9 @@
 ﻿using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +14,13 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        // If using SendGrid to send emails
+        //public string SendGridSecret { get; set; }
+        //public EmailSender(IConfiguration _config)
+        //{
+        //    SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        //}
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             var emailToSend = new MimeMessage();
@@ -30,6 +40,14 @@ namespace BulkyBook.Utility
             }
 
             return Task.CompletedTask;
+
+            // If using SendGrid to send emails
+            //var client = new SendGridClient(SendGridSecret);
+            //var from = new EmailAddress("test@company.com", "Bulky Book");
+            //var to = new EmailAddress(email);
+            //var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            //return client.SendEmailAsync(msg);
         }
     }
 }
