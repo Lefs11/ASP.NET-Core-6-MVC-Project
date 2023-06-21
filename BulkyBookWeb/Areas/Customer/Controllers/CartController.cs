70007ca38b80dc50cdb80444e387e88a7cee5b56
Liliana Ferrer <40367237+Lefs11@users.noreply.github.com>
2023-06-21 15:27:57 -0600

    Validation for admin roles added

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -195,6 +195,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
 
+            HttpContext.Session.Clear();
             _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
             _unitOfWork.Save();
             return View(id);
